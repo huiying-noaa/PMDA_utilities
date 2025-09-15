8ef68f6fdbdadf02157ad705456d5e2909e5e765
huiying-noaa <huiying.luo@noaa.gov>
2025-09-14 22:57:02 -0600

    Correct Ptop and clean code

--- Add_dryairdensity/PMDA_add_dryairdensity.py
+++ Add_dryairdensity/PMDA_add_dryairdensity.py
@@ -1,77 +1,71 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# By Huiying Luo, huiying.luo@noaa.gov, Sept 2025, published v1
+# By Huiying Luo, huiying.luo@noaa.gov, Sept 2025, published v1.1
 
-## Script to add/update dry air density variable to background core file for PM2.5 DA 
-## How to: 
-    # python/run (depends on where the script will be run) script_name background_core_file save_original_copy_switch(optional, 0/1. default value: 1, save copy; 0, not save copy) 
+## Script to add/update dry air density variable to RRFS FV3 background core file for PM2.5 DA
+## How to:
+    # python/run script_name background_core_file optional_save_original_core_switch (Default value: 1, save copy. Option 0, not save copy)
     # e.g.: python PMDA_add_dryairdensity.py '/scratch2/BMC/wrfruc/hluo/PM3dvar/3dvar_c84/Data/inputs/lam_rrfs/bkg/sd_20200912.120000.fv_core.res.tile1test.nc'
-## IMPORTANT NOTE: Script will change background core file directly! Please copy example data before testing!
-    
+## IMPORTANT NOTE: Script will change background core file directly if switch set to 0! Please copy example data before testing that option!
+
 import netCDF4 as nc
 import numpy as np
 import sys
 import shutil
 
-## Input. Pending change: option to read core file name from yaml file 
-filecore = sys.argv[1] 
+## Input
+filecore = sys.argv[1]
 
 if len(sys.argv)==3 and sys.argv[2]==0:
     print('NOTE: Core file will be modified!')
 else:
     shutil.copyfile(filecore, filecore+'.copy')
-    print('Core file copy saved.')
+    print('Core file copy saved to '+filecore+'.copy')
 
-print('Changing core file...')
+print('Calculating dry air density for '+ filecore)
 
-## read and prepare data
+## Read and prepare data
 datacore =nc.Dataset(filecore,'r+')
 
 delp = datacore.variables['delp'][:] #air pressure thickness (Pa), from top to surface; Ptop 20mb to 2000 Pa
-T = datacore.variables['T'][:] 
+T = datacore.variables['T'][:]
 delp = np.ma.masked_where(delp == 9.969209968386869e+36, delp)
 T = np.ma.masked_where(T == 9.969209968386869e+36, T)
-print(delp[0,:,0,0])
+# print(delp[0,:,0,0])
 
-## calculation
+## Calculation
 # v0: rough estimate P=np.cumsum(delp, axis=1)
-P=np.cumsum(delp, axis=1)
-#print(P[0,:,0,0])
+# P=np.cumsum(delp, axis=1)
+# print(P[0,:,0,0])
 
 # v1: use ptop to get a more precise estimate. P at center ~ average of top and bottom, validated using Pcenter.py
-ptop=2000 #Pa
+ptop=212.637 #Pa for fv3
+print('Note: Used Ptop at 212.637 Pa for RRFS FV3!')
 P=np.zeros(delp.shape)
 Pcum=np.cumsum(delp, axis=1)
 P[0,0,:,:]=ptop+delp[0,0,:,:]/2
 for layer in range(1,delp.shape[1]):
     P[0,layer,:,:]=ptop+Pcum[0,layer-1,:,:]+delp[0,layer,:,:]/2
-print(P[0,:,0,0])
+# print(P[0,:,0,0])
 
 
-Rconst = 287.05 #J/(kg·K), dens (1.225 kg/m3) = P (101325 Pa)/(Rconst * T (k)) 
+Rconst = 287.05 #J/(kg·K), dens (1.225 kg/m3) = P (101325 Pa)/(Rconst * T (k))
 denstest = P / (T * Rconst)
 
-# print(P[0,:,0,0])
-print(np.max(denstest[0,0,:,:]))
-print(np.min(denstest[0,0,:,:]))
-print(np.max(denstest[0,-1,:,:]))
-print(np.min(denstest[0,-1,:,:]))
+## Validations
+# print(np.max(denstest[0,0,:,:]))
+# print(np.min(denstest[0,0,:,:]))
+# print(np.max(denstest[0,-1,:,:]))
+# print(np.min(denstest[0,-1,:,:]))
 
-# Validation A: standard atm https://www.eoas.ubc.ca/courses/atsc113/flying/met_concepts/02-met_concepts/02a-std_atmos-P/index.html#:~:text=The%20table%20above%20shows%20that,bottom%20quarter%20of%20the%20photo).
-# density 0.0395 kg/m3, P 2.511 kPa, T -51.5C, H 25 km
-# V1 similar to std atm, not the v0/ref from Hongli
+# Ref: standard atm Table 1-5 from https://www.eoas.ubc.ca/courses/atsc113/flying/met_concepts/02-met_concepts/02a-std_atmos-P/index.html#:~:text=The%20table%20above%20shows%20that,bottom%20quarter%20of%20the%20photo).
 
-# Validation B: monet first layer dense in /mnt/lfs6/BMC/wrfruc/hluo/workflow/cleanrun/PMDA_c/stmp/monet/dynf_2024072721_001.nc to validate
-
-## add variable to core
+## Add variable to core
 if 'dry_air_density' in datacore.variables.keys():
-
     #datacore['dry_air_density'][:] = denstest.astype('float32') # doesn't work for the inital test density w wrong dim
-    
-    
-    for i in range(100): 
+    for i in range(100):
         try:
-            datacore.renameVariable('dry_air_density','dry_air_density_'+str(i)) # rename original to _i if exist 
+            datacore.renameVariable('dry_air_density','dry_air_density_'+str(i)) # rename original to _i if exist
             print('Previous estimate saved to V '+ str(i))
             break
         except:
